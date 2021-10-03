13702f5e3fd949c354eed9e15b938e2020cc1168
Matthew N <91503895+MatthewNortonPersonal@users.noreply.github.com>
2021-10-03 17:18:04 +0000

    I created all of the methods and declared the instance variables and made the constructor.

--- Car.java
+++ Car.java
@@ -2,63 +2,85 @@ import java.util.Scanner;
 
 public class Car {
 
-    public Car(String model, double mpg, double gasTankCapacity) {
+  String model;
+  double mpg;
+  double gasTankCapacity;
+  double gasRemaining;
 
-    }
+  public Car(String model, double mpg, double gasTankCapacity) {
+    this.model = model;
+    this.mpg = mpg;
+    this.gasTankCapacity = gasTankCapacity;
+    this.gasRemaining = 0;
+    // "this" refers to a generic object that we do not know the name of yet. This is useful for when we need to assign an instance variable for an object when there are parameters.
 
-    public static void main(String[] args) {
-        // Create a new Car object, asking the user (using the scanner class) for the model, mpg, and gasTankCapacity
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Model: ");
-        String model = sc.nextLine();
-        System.out.print("MPG: ");
-        double mpg = sc.nextDouble();
-        System.out.print("Gas Tank Capacity: ");
-        double gasTankCapacity = sc.nextDouble();
-        Car car = new Car(model, mpg, gasTankCapacity);
+  }
 
-        // Refuel the car to its maximum gas capacity
-        car.refuel(gasTankCapacity);
+  // I don't need to modify main
 
-        // Print out car information
-        System.out.println(car);
+  public static void main(String[] args) {
+    // Create a new Car object, asking the user (using the scanner class) for the model, mpg, and gasTankCapacity
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Model: ");
+    String model = sc.nextLine();
+    System.out.print("MPG: ");
+    double mpg = sc.nextDouble();
+    System.out.print("Gas Tank Capacity: ");
+    double gasTankCapacity = sc.nextDouble();
+    Car car = new Car(model, mpg, gasTankCapacity);
 
-        // Ask the user how many miles they want to drive, then drive that distance, and then report back the car's remaining gas and range after the drive.
-        System.out.print("How far do you want to drive? ");
-        double miles = sc.nextDouble();
-        car.drive(miles);
-        System.out.println("Remaining Gas: " + car.getGasRemaining() + ", Remaining Range: " + car.getRange());
+    // Refuel the car to its maximum gas capacity
+    car.refuel(gasTankCapacity);
 
-        // Ask the user how much gas they want to add, and then refuel the car that amount
-        System.out.print("How much gas do you want to add? ");
-        double gas = sc.nextDouble();
-        car.refuel(gas);
+    // Print out car information
+    System.out.println(car);
 
-        // Ask the user how many miles they want to drive, then drive that distance, and then report back the car's remaining gas and range after the drive.
-        System.out.print("How far do you want to drive? ");
-        miles = sc.nextDouble();
-        car.drive(miles);
-        System.out.println("Remaining Gas: " + car.getGasRemaining() + ", Remaining Range: " + car.getRange());
-    }
+    // Ask the user how many miles they want to drive, then drive that distance, and then report back the car's remaining gas and range after the drive.
+    System.out.print("How far do you want to drive? ");
+    double miles = sc.nextDouble();
+    car.drive(miles);
+    System.out.println("Remaining Gas: " + car.getGasRemaining() + ", Remaining Range: " + car.getRange());
 
-    public void drive(double distance) {
-        // drive for distance (in miles), and update gas tank level accordingly (using mpg)
-    }
+    // Ask the user how much gas they want to add, and then refuel the car that amount
+    System.out.print("How much gas do you want to add? ");
+    double gas = sc.nextDouble();
+    car.refuel(gas);
 
-    public void refuel(double gasAmount) {
-        // Add gasAmount of gas to the gas tank
-    }
+    // Ask the user how many miles they want to drive, then drive that distance, and then report back the car's remaining gas and range after the drive.
+    System.out.print("How far do you want to drive? ");
+    miles = sc.nextDouble();
+    car.drive(miles);
+    System.out.println("Remaining Gas: " + car.getGasRemaining() + ", Remaining Range: " + car.getRange());
+  }
 
-    public double getGasRemaining() {
-        // Return the number of gallons of gas currently in the gas tank
-    }
+  // I don't need to modify main
 
-    public double getRange() {
-        // Return the estimated amount of miles the car can drive based on current gas remaining and MPG (provided to constructor)
-    }
+  public void drive(double distance) {
+    // drive for distance (in miles), and update gas tank level accordingly (using mpg)
+    gasRemaining = gasRemaining - distance / mpg;
 
-    public String toString() {
-        // Return a string representation of the car's current state.
-        // Be sure to include range, model, and gas level.
-    }
+  }
+
+  public void refuel(double gasAmount) {
+    // Add gasAmount of gas to the gas tank
+    gasRemaining += gasAmount;
+    
+  }
+
+  public double getGasRemaining() {
+    // Return the number of gallons of gas currently in the gas tank
+    return gasRemaining;
+  }
+
+  public double getRange() {
+    // Return the estimated amount of miles the car can drive based on current gas remaining and MPG (provided to constructor)
+    return mpg * gasRemaining;
+  }
+
+  public String toString() {
+    // Return a string representation of the car's current state.
+    // Be sure to include range, model, and gas level.
+
+    return "Range: " + getRange() + " miles. " + "Model: " + model + ". " + "Gas Level: " + getGasRemaining() + " gallons.";
+  }
 }
